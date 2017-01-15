9a46b13b2f68459206463064e4a74fea74f85121
4hel <4hel@posteo.de>
2017-01-15 17:35:52 +0100

    dup1

---
+++ chap01/g_dup1/main.go
@@ -0,0 +1,23 @@
+//Dup1 check for duplicate lines and print with count
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	// NOTE: ignoring error from input.Err()
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
