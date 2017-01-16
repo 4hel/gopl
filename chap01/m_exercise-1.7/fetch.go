324794fb6aa390dcb96114860147015a20f2e42b
4hel <4hel@posteo.de>
2017-01-16 15:22:45 +0100

    fetch with io.Copy

---
+++ chap01/m_exercise-1.7/fetch.go
@@ -0,0 +1,24 @@
+// Fetch prints the contents from an url using io.Copy to stdout
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		_, err = io.Copy(os.Stdout, resp.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+	}
+}
