27b07f60b26101e843cb0b9c79ef2a278e3ec192
4hel <4hel@posteo.de>
2017-01-16 15:42:26 +0100

    fetch adding prefix http://

---
+++ chap01/n_exercise-1.7b/fetch.go
@@ -0,0 +1,28 @@
+// Fetch prints the contents from an url using io.Copy to stdout
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		_, err = io.Copy(os.Stdout, resp.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+	}
+}
