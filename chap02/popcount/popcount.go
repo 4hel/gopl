b977ffb8b192f7d20784a8aee4bec464f38ebc11
4hel <4hel@posteo.de>
2017-01-18 19:37:23 +0100

    population count

---
+++ chap02/popcount/popcount.go
@@ -0,0 +1,23 @@
+// Couting the number of bits set to 1 in number (population)
+package popcount
+
+// pc[i] is the population for the number i
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+// PopCount return the population count (number of set bits) of x.
+func PopCount(x uint64) int {
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+}
---
+++ chap02/popcount/popcount_test.go
@@ -0,0 +1,31 @@
+package popcount
+
+import (
+	"testing"
+)
+
+func TestPopCount(t *testing.T) {
+	t.Log("testing PopCount function...")
+
+	// 0x01 -> 1 bit set
+	result := PopCount(1)
+	if result != 1 {
+		t.Errorf("expected 1 bit set for (int 1) but got %d", result)
+	} else {
+		t.Log("1 bit set for (int 1)")
+	}
+
+	// 0x05 -> 2 bits set
+	result = PopCount(0x0500)
+	if result != 2 {
+		t.Errorf("expected 2 bits set for 0x0500 but got %d", result)
+	} else {
+		t.Log("2 bits set for 0x0500")
+	}
+}
+
+func BenchmarkPopCount(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCount(uint64(i))
+	}
+}
