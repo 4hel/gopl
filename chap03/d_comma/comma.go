504c3b1966e182940768eac03f50ddc2fb25a1db
4hel <4hel@posteo.de>
2017-01-20 13:53:43 +0100

    comma to numbers

---
+++ chap03/d_comma/comma.go
@@ -0,0 +1,22 @@
+// Comma inserts commas into positive integers
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	for _, arg := range os.Args[1:] {
+		fmt.Println(comma(arg))
+	}
+}
+
+func comma(s string) string {
+	n := len(s)
+	if n < 3 {
+		return s
+	} else {
+		return comma(s[:n-3]) + "," + s[n-3:]
+	}
+}
