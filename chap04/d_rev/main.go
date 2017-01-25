b46ffe19f30943e6efb4d0477e8a085b8e876883
4hel <4hel@posteo.de>
2017-01-25 10:43:59 +0100

    reverse slice

---
+++ chap04/d_rev/main.go
@@ -0,0 +1,18 @@
+// Rev reverses a slice.
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := [...]int{0, 1, 2, 3, 4, 5}
+	reverse(a[:])
+	fmt.Println(a) // "[5 4 3 2 1 0]
+}
+
+func reverse(s []int) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
