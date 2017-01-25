27c596e96d8569b8f0d80fefaec29adb4ed8b24a
4hel <4hel@posteo.de>
2017-01-25 16:11:25 +0100

    count character classes

---
+++ chap04/j_charclass/main.go
@@ -0,0 +1,58 @@
+// Charcount computes counts of Unicode classes like Letter, Digit, etc
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"unicode"
+	"unicode/utf8"
+)
+
+func main() {
+	counts := make(map[string]int)  // counts of Unicode characters
+	var utflen [utf8.UTFMax + 1]int // count of lengths of UTF-8 encodings
+	invalid := 0                    // count of invalid UTF-8 characters
+
+	in := bufio.NewReader(os.Stdin)
+	for {
+		r, n, err := in.ReadRune() // returns rune, nbytes, error
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "charcount: %v\n", err)
+			os.Exit(1)
+		}
+		if r == unicode.ReplacementChar && n == 1 {
+			invalid++
+			continue
+		}
+		if unicode.IsLetter(r) {
+			counts["letter"]++
+		} else if unicode.IsNumber(r) {
+			counts["number"]++
+		} else if unicode.IsSpace(r) {
+			counts["space"]++
+		} else {
+			counts["other"]++
+		}
+		utflen[n]++
+	}
+	fmt.Printf("type:\tcount:\n")
+	for c, n := range counts {
+		fmt.Printf("%s\t%d\n", c, n)
+	}
+	fmt.Print("\nlen\tcount\n")
+	for i, n := range utflen {
+		if i > 0 {
+			fmt.Printf("%d\t%d\n", i, n)
+		}
+	}
+	if invalid > 0 {
+		fmt.Printf("\n%d invalid UTF-8 characters\n", invalid)
+	}
+}
+
+//!-
