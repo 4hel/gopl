4bba71e52adb3822120a293e013e1fe2a8498be9
4hel <4hel@posteo.de>
2017-01-31 17:12:44 +0100

    findlinks1 + additionally find other links like images and scripts

---
+++ chap05/e_exercise-5.4/main.go
@@ -0,0 +1,40 @@
+// findlinks1 + additionally find other links like images and scripts
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/html"
+	"os"
+)
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
+		os.Exit(1)
+	}
+	for _, link := range visit(nil, doc) {
+		fmt.Println(link)
+	}
+}
+
+func visit(links []string, n *html.Node) []string {
+	if n.Type == html.ElementNode && (n.Data == "a" || n.Data == "link") {
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				links = append(links, a.Val)
+			}
+		}
+	}
+	if n.Type == html.ElementNode && n.Data == "img" {
+		for _, a := range n.Attr {
+			if a.Key == "src" {
+				links = append(links, a.Val)
+			}
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		links = visit(links, c)
+	}
+	return links
+}
