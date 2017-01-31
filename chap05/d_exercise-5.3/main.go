8f084e51e8420f3706ff797110963327660692db
4hel <4hel@posteo.de>
2017-01-31 16:51:35 +0100

    parse html TextNodes and print to stdout

---
+++ chap05/d_exercise-5.3/main.go
@@ -0,0 +1,39 @@
+// Findlinks1 prints the links in an HTML document read from standard input.
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/html"
+	"os"
+	"unicode"
+)
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
+		os.Exit(1)
+	}
+	visit(doc)
+}
+
+func visit(n *html.Node) {
+	if n.Type == html.TextNode && n.Parent.Data != "script" {
+		if containsNonWhiteSpace([]rune(n.Data)) {
+			fmt.Print(n.Data, " ")
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		visit(c)
+	}
+}
+
+func containsNonWhiteSpace(s []rune) bool {
+	retval := false
+	for _, c := range s {
+		if !unicode.IsSpace(c) {
+			retval = true
+		}
+	}
+	return retval
+}
---
+++ chap05/d_exercise-5.3/run.sh
@@ -0,0 +1,1 @@
+curl -s https://golang.org | go run main.go
