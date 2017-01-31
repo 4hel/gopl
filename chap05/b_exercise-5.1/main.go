8194a89ce4a6aa59ee4e998ec1e24f2220820863
4hel <4hel@posteo.de>
2017-01-31 16:53:49 +0100

    findlinks without for loop

--- chap05/b_exercise-5.1/main.go
+++ chap05/b_exercise-5.1/main.go
@@ -1,10 +1,4 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-
-// See page 122.
-//!+main
-
-// Findlinks1 prints the links in an HTML document read from standard input.
+// Findlinks witout for loop
 package main
 
 import (
@@ -25,10 +19,6 @@ func main() {
 	}
 }
 
-//!-main
-
-//!+visit
-// visit appends to links each link found in n and returns the result.
 func visit(links []string, n *html.Node) []string {
 	if n.Type == html.ElementNode && n.Data == "a" {
 		for _, a := range n.Attr {
@@ -47,35 +37,3 @@ func visit(links []string, n *html.Node) []string {
 	}
 	return links
 }
-
-//!-visit
-
-/*
-//!+html
-package html
-
-type Node struct {
-	Type                    NodeType
-	Data                    string
-	Attr                    []Attribute
-	FirstChild, NextSibling *Node
-}
-
-type NodeType int32
-
-const (
-	ErrorNode NodeType = iota
-	TextNode
-	DocumentNode
-	ElementNode
-	CommentNode
-	DoctypeNode
-)
-
-type Attribute struct {
-	Key, Val string
-}
-
-func Parse(r io.Reader) (*Node, error)
-//!-html
-*/
