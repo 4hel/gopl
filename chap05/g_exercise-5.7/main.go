42357f56ec4f03b6c492b693ec59242ac2995173
4hel <4hel@posteo.de>
2017-02-06 10:07:33 +0100

    pretty printing the parsed html

--- chap05/g_exercise-5.7/main.go
+++ chap05/g_exercise-5.7/main.go
@@ -2,18 +2,12 @@ package main
 
 import (
 	"fmt"
-
-	"os"
-
 	"golang.org/x/net/html"
+	"os"
+	"strings"
 )
 
 func main() {
-	/*
-		for _, url := range os.Args[1:] {
-			outline(url)
-		}
-	*/
 	doc, err := html.Parse(os.Stdin)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
@@ -22,32 +16,6 @@ func main() {
 	forEachNode(doc, startElement, endElement)
 }
 
-/*
-func outline(url string) error {
-	resp, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	doc, err := html.Parse(resp.Body)
-	if err != nil {
-		return err
-	}
-
-	//!+call
-	forEachNode(doc, startElement, nil)
-	//!-call
-
-	return nil
-}
-*/
-
-//!+forEachNode
-// forEachNode calls the functions pre(x) and post(x) for each node
-// x in the tree rooted at n. Both functions are optional.
-// pre is called before the children are visited (preorder) and
-// post is called after (postorder).
 func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
 	if pre != nil {
 		pre(n)
@@ -62,29 +30,34 @@ func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
 	}
 }
 
-//!-forEachNode
-
-//!+startend
 var depth int
 
 func startElement(n *html.Node) {
 	if n.Type == html.ElementNode {
-		//fmt.Printf("%*s<%s>\n", depth*2, "", n.Data)
-		printNodeStart(n)
+		printElementNodeStart(n)
 		depth++
+	} else if n.Type == html.CommentNode {
+		printCommentNodeStart(n)
+		depth++
+	} else if n.Type == html.TextNode {
+		printTextNode(n)
+		//depth++
 	}
 }
 
 func endElement(n *html.Node) {
 	if n.Type == html.ElementNode {
 		depth--
-		if n.FirstChild != nil {
-			fmt.Printf("%*s</%s>\n", depth*2, "", n.Data)
-		}
+		printElementNodeEnd(n)
+	} else if n.Type == html.CommentNode {
+		depth--
+		printCommentNodeEnd(n)
+	} else if n.Type == html.TextNode {
+		//depth--
 	}
 }
 
-func printNodeStart(n *html.Node) {
+func printElementNodeStart(n *html.Node) {
 	fmt.Printf("%*s<%s", depth*2, "", n.Data)
 	for _, a := range n.Attr {
 		fmt.Printf(" %s=\"%s\"", a.Key, a.Val)
@@ -96,4 +69,23 @@ func printNodeStart(n *html.Node) {
 	}
 }
 
-//!-startend
+func printElementNodeEnd(n *html.Node) {
+	if n.FirstChild != nil {
+		fmt.Printf("%*s</%s>\n", depth*2, "", n.Data)
+	}
+}
+
+func printCommentNodeStart(n *html.Node) {
+	fmt.Printf("<!--%s", n.Data)
+}
+
+func printCommentNodeEnd(n *html.Node) {
+	fmt.Printf("-->\n")
+}
+
+func printTextNode(n *html.Node) {
+	trimmed := strings.TrimSpace(n.Data)
+	if trimmed != "" {
+		fmt.Printf("%*s%s\n", depth*2, "", trimmed)
+	}
+}
