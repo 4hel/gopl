4be72f83b168c6a2ee2a95f6ecfa848a0b3d8029
4hel <4hel@posteo.de>
2017-02-14 06:59:29 +0100

    tempconv command line - celsiusFlag satisfies flag.Value interface

---
+++ chap07/f_tempflag/tempflag.go
@@ -0,0 +1,23 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 181.
+
+// Tempflag prints the value of its -temp (temperature) flag.
+package main
+
+import (
+	"./tempconv"
+	"flag"
+	"fmt"
+)
+
+//!+
+var temp = tempconv.CelsiusFlag("temp", 20.0, "the temperature")
+
+func main() {
+	flag.Parse()
+	fmt.Println(*temp)
+}
+
+//!-
