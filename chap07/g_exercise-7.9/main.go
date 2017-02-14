90e2660ac510e6074ae2d4c4daf8b2ab50c33f92
4hel <4hel@posteo.de>
2017-02-14 09:59:36 +0100

    single sort works

---
+++ chap07/g_exercise-7.9/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"sort"
+	"time"
+)
+
+type Track struct {
+	Title  string
+	Artist string
+	Album  string
+	Year   int
+	Length time.Duration
+}
+
+var tracks = []*Track{
+	{"Go", "Delilah", "From the Roots Up", 2012, length("3m38s")},
+	{"Go", "Moby", "Moby", 1992, length("3m37s")},
+	{"Go Ahead", "Alicia Keys", "As I Am", 2007, length("4m36s")},
+	{"Ready 2 Go", "Martin Solveig", "Smash", 2007, length("4m24s")},
+}
+
+func length(s string) time.Duration {
+	d, err := time.ParseDuration(s)
+	if err != nil {
+		panic(s)
+	}
+	return d
+}
+
+type byYear []*Track
+
+func (x byYear) Len() int           { return len(x) }
+func (x byYear) Less(i, j int) bool { return x[i].Year < x[j].Year }
+func (x byYear) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
+
+type byArtist []*Track
+
+func (x byArtist) Len() int           { return len(x) }
+func (x byArtist) Less(i, j int) bool { return x[i].Artist < x[j].Artist }
+func (x byArtist) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
+
+var html = template.Must(template.ParseFiles("tracks.html"))
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	var sorting sort.Interface
+
+	if err := r.ParseForm(); err != nil {
+		log.Fatal(err)
+	}
+
+	if len(r.Form["sort"]) > 0 {
+		switch r.Form["sort"][0] {
+		case "year":
+			sorting = byYear(tracks)
+
+		case "artist":
+			sorting = byArtist(tracks)
+		}
+	}
+
+	if sorting != nil {
+		sort.Sort(sorting)
+	}
+
+	if err := html.Execute(w, tracks); err != nil {
+		log.Fatal(err)
+	}
+}
