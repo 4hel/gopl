7ddd60a94b3283cb8534f6a1fa6f8eb2648a8192
4hel <4hel@posteo.de>
2017-02-16 12:41:22 +0100

    update func adapted to http.Handler interface using DefaultServeMux

---
+++ chap07/h_exercise-7.11/main.go
@@ -0,0 +1,72 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 195.
+
+// Http4 is an e-commerce server that registers the /list and /price
+// endpoint by calling http.HandleFunc.
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+//!+main
+
+func main() {
+	db := database{"shoes": 50, "socks": 5}
+	http.HandleFunc("/list", db.list)
+	http.HandleFunc("/price", db.price)
+	http.HandleFunc("/update", db.update)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+//!-main
+
+type dollars float32
+
+func (d dollars) String() string { return fmt.Sprintf("$%.2f", d) }
+
+type database map[string]dollars
+
+func (db database) update(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	price := req.URL.Query().Get("price")
+	if _, ok := db[item]; ok {
+		f, err := strconv.ParseFloat(price, 64)
+		if err == nil {
+			if f > 0 {
+				db[item] = dollars(f)
+				fmt.Fprintf(w, "ok\n")
+			} else {
+				w.WriteHeader(http.StatusBadRequest) // 400
+				fmt.Fprintf(w, "price must be > 0: %f\n", f)
+			}
+		} else {
+			w.WriteHeader(http.StatusBadRequest) // 400
+			fmt.Fprintf(w, "cannot parse price: %v\n", err)
+		}
+	} else {
+		w.WriteHeader(http.StatusNotFound) // 404
+		fmt.Fprintf(w, "no such item: %q\n", item)
+	}
+}
+
+func (db database) list(w http.ResponseWriter, req *http.Request) {
+	for item, price := range db {
+		fmt.Fprintf(w, "%s: %s\n", item, price)
+	}
+}
+
+func (db database) price(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	if price, ok := db[item]; ok {
+		fmt.Fprintf(w, "%s\n", price)
+	} else {
+		w.WriteHeader(http.StatusNotFound) // 404
+		fmt.Fprintf(w, "no such item: %q\n", item)
+	}
+}
